a8f00dbd1d2f0897e122351d65f30f4ba1b95740
b0vdurr <163833887+b0vdurr@users.noreply.github.com>
2025-04-08 17:15:57 +0200

    Dialogue box and answer options

--- resources/views/office.blade.php
+++ resources/views/office.blade.php
@@ -3,27 +3,45 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+          content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     <title>Document</title>
 </head>
 <body>
-    <style>
-        body{
-            background-image: url('{{ asset('images/background.png') }}');
-            background-size: cover;
-            overflow: hidden;
-        }
-    </style>
-    <main>
+<style>
+    body {
+        background-image: url('{{ asset('images/background.png') }}');
+        background-size: cover;
+        background-repeat: no-repeat;
+        background-position: center;
+    }
+</style>
 
-        <div class="patient">
-            <img src="{{asset('images/old-man.png') }}" alt="" class="patient-img">
+<main>
+    <div class="patient">
+        <img src="{{ asset('images/old-man.png') }}" alt="Old man">
+    </div>
+
+    <div class="dialogue">
+        <div class="dialogue-arrow"></div>
+        <div class="dialogue-box">
+            <i>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Lorem. lorem</i>
+        </div>
+    </div>
+
+    <div class="answer">
+        <div class="answer-option">
+            1. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores culpa cupiditate exercitationem facilis harum illum ipsam maxime mollitia nam neque nisi praesentium quam ratione, repellat saepe temporibus, totam voluptatem voluptates?
+        </div>
+        <div class="answer-option">
+            2. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facilis incidunt itaque iusto laboriosam
+                maxime minus, nobis praesentium. Assumenda at doloribus, ducimus esse nostrum quibusdam repellat
+                repellendus suscipit vero vitae! Doloribus!
         </div>
 
-    </main>
-    <x-items-box />
 
+    </div>
+</main>
 </body>
 </html>
