047c676e0c2d07edb29da507244c8860ee445cbb
Ilya Odnoral <99083003@mydavinci.nl>
2025-04-13 16:47:14 +0200

    Menu; fresh styles; items-box close button

--- resources/views/components/items-box.blade.php
+++ resources/views/components/items-box.blade.php
@@ -1,6 +1,21 @@
-<div class="items-box">
-    <img src="{{asset('images/items-icons/blood.png')}}" alt="blood">
-    <img src="{{asset('images/items-icons/MRI.png')}}" alt="MRI">
-    <img src="{{asset('images/items-icons/Pills.png')}}" alt="Pills">
-    <img src="{{asset('images/items-icons/UZI.png')}}" alt="UZI">
+<div class="items-box-close-button" id = "itemsBoxCloseButton">
+    <span class="items-bar"></span>
+</div>
+<div class="items-box" id = "itemsBox">
+    <div class="tooltip">
+        <img src="{{ asset('images/items-icons/blood.png') }}" alt="blood">
+        <span class="tooltip-text">Bloed</span>
+    </div>
+    <div class="tooltip">
+        <img src="{{ asset('images/items-icons/MRI.png') }}" alt="MRI">
+        <span class="tooltip-text">MRI</span>
+    </div>
+    <div class="tooltip">
+        <img src="{{ asset('images/items-icons/Pills.png') }}" alt="Pills">
+        <span class="tooltip-text">Paracetomol</span>
+    </div>
+    <div class="tooltip">
+        <img src="{{ asset('images/items-icons/UZI.png') }}" alt="UZI">
+        <span class="tooltip-text">UZI</span>
+    </div>
 </div>
--- resources/views/office.blade.php
+++ resources/views/office.blade.php
@@ -11,31 +11,50 @@
 <body>
 <style>
     body {
-        background-image: url('{{ asset('images/background.png') }}');
+        background-image: url('{{ asset('images/Kantoor.png') }}');
         background-size: cover;
         background-repeat: no-repeat;
         background-position: center;
+        overflow: hidden;
     }
 </style>
 
 <main>
+    {{--MENU BUTTON--}}
+    <button class="menu-btn" id="menuButton">
+        <span class="bar top"></span>
+        <span class="bar middle"></span>
+        <span class="bar bottom"></span>
+    </button>
+    {{--MENU--}}
+    <div class="menu" id="menu">
+        <a href="{{ route('home') }}" class="menu-item">Go back to menu</a>
+        <button class="menu-item" id="openSettings">Settings</button>
+        <button class="menu-item">Audio on/off</button>
+    </div>
+    {{--PATIENT IMG--}}
     <div class="patient">
         <img src="{{ asset('images/old-man.png') }}" alt="Old man">
     </div>
 
+    {{--DIALOG--}}
     <div class="dialogue-wrapper">
 
         <div class="dialogue">
-            <div class="dialogue-box">lorum ipsum dolor sit amet, consectetur adipisicing elit. Ab accusantium ad alias aliquid amet animi</div>
+            <div class="dialogue-box">Dokter, ik heb al weken rugpijn... Is het gewoon ouderdom?</div>
             <div class="dialogue-arrow"></div>
         </div>
         <div class="answer">
-            <div class="answer-option">  lorum ipsum dolor sit amet, consectetur adipisicing elit. Ab accusantium ad alias aliquid amet animi</div>
-            <div class="answer-option">  lorum ipsum dolor sit amet, consectetur adipisicing elit. Ab accusantium ad alias aliquid amet animi</div>
-            <div class="answer-option">  lorum ipsum dolor sit amet, consectetur adipisicing elit. Ab accusantium ad alias aliquid amet animi</div>
+            <div class="answer-option">Het kan slijtage zijn van de wervels. We maken een scan om dat te zien. </div>
         </div>
     </div>
+
+    {{--ITEMS BOX--}}
     <x-items-box />
+
+
 </main>
+
 </body>
+
 </html>
