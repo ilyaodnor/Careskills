653f7ef35965b1baa0ac2d7babcce4791d4afd27
Ilya Odnoral <99083003@mydavinci.nl>
2025-05-07 11:00:46 +0200

    Home page #3

--- routes/web.php
+++ routes/web.php
@@ -3,22 +3,14 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-
 Route::get('/', function () {
-    return view('home');
-});
-Route::get('/office', function () {
-    return view('office');
-})->name('office');
-Route::get('/home', function () {
     return view('home');
 })->name('home');
 
-
-
-//Route::get('/office', function () {
-//    return view('office');
-//})->middleware(['auth', 'verified'])->name('office');
+Route::get('/office', function () {
+    return view('office');
+})->name('office');
+//    ->middleware(['auth', 'verified'])->name('office');
 //
 //Route::middleware('auth')->group(function () {
 //    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ['resources/css/office.css', 'resources/js/office.js'],
             refresh: true,
         }),
     ],
