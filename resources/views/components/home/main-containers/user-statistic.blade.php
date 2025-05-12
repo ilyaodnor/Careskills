1a1bc0dad199e521c7f872ef5ae54d082e3e8527
Davyd Tskhovrebov <99080640@mydavinci.nl>
2025-05-12 11:28:54 +0200

    Correct user's info output

--- resources/views/components/home/main-containers/user-statistic.blade.php
+++ resources/views/components/home/main-containers/user-statistic.blade.php
@@ -3,17 +3,12 @@
     @if (auth()->check() && auth()->user()->profile_photo_path)
         <img src="{{ auth()->user()->profile_photo_url }}" alt="User Photo" class="user-photo">
     @else
-        <img src="{{ asset('CareskillsAssets/users-photos/default-user-photo.png') }}" alt="Default Male Photo" class="user-photo">
+        <a href="{{route('profile.edit')}}"><img src="{{ asset('CareskillsAssets/users-photos/default-user-photo.png') }}" alt="Default Male Photo" class="user-photo"></a>
     @endif
 
-    @if (auth()->check())
-        <h1>{{ auth()->user()->name ?? "Error" }}</h1>
-    @else
-        <h1></h1>
-    @endif
     <div class="user__info">
-        <h1 class = "user__name">{{ $name ?? 'Name Имя' }}</h1>
-        <h2 class = "user__level">{{ $level ?? "100Lvl" }}</h2>
+        <h1 class = "user__name">{{ auth()->user()->name }}</h1>
+        <h2 class = "user__level">100Lvl</h2>
         <div class="user-xp-container">
             <div class="xp-bar">
                 <div class="xp-bar-filled"></div>
